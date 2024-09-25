3a15e706ed659aa6a17ea2f8e0becab8cf4e8324
Michael-Naidoo <130663291+Michael-Naidoo@users.noreply.github.com>
2024-09-25 15:11:18 +0200

    added 2 wheel functionality

--- Group5_HillClimbingChickenHorse/Assets/Scripts/CharacterControlerBasic.cs
+++ Group5_HillClimbingChickenHorse/Assets/Scripts/CharacterControlerBasic.cs
@@ -10,7 +10,17 @@ namespace DefaultNamespace
 
         public float speed;
         public float maxAcceleration;
+        public float wrongWheelMultiplier;
+        
+        public enum WheelPosition
+        {
+            Front,
+            Back
+        }
 
+        public WheelPosition wheelPosition;
+
+        private bool grounded;
         private void Start()
         {
             rB2D = GetComponent<Rigidbody2D>();
@@ -19,10 +29,49 @@ namespace DefaultNamespace
 
         private void Update()
         {
-            if (rB2D.velocity.magnitude < speed)
+            if (grounded)
             {
-                rB2D.AddForce(new Vector2(pI.throttle * maxAcceleration * Time.deltaTime,0), ForceMode2D.Force);
+                switch (wheelPosition)
+                {
+                    case WheelPosition.Front:
+                        if (rB2D.velocity.magnitude < speed && pI.throttle < 0)
+                        {
+                            rB2D.AddForce(new Vector2(pI.throttle * maxAcceleration * Time.deltaTime,0), ForceMode2D.Force);
+                        }
+                        else if (rB2D.velocity.magnitude < speed)
+                        {
+                            rB2D.AddForce(new Vector2(pI.throttle * wrongWheelMultiplier * maxAcceleration * Time.deltaTime,0), ForceMode2D.Force);
+                        }
+                        return;
+                    case WheelPosition.Back:
+                        if (rB2D.velocity.magnitude < speed && pI.throttle > 0)
+                        {
+                            rB2D.AddForce(new Vector2(pI.throttle * maxAcceleration * Time.deltaTime,0), ForceMode2D.Force);
+                        }
+                        else if (rB2D.velocity.magnitude < speed)
+                        {
+                            rB2D.AddForce(new Vector2(pI.throttle * wrongWheelMultiplier * maxAcceleration * Time.deltaTime,0), ForceMode2D.Force);
+                        }
+                        return;
+                }
             }
+        }
+
+        private void OnCollisionStay2D(Collision2D col)
+        {
+            if (col.gameObject.CompareTag("Ground"))
+            {
+                grounded = true;
+            }
+            else
+            {
+                grounded = false;
+            }
+        }
+
+        private void OnCollisionExit2D(Collision2D other)
+        {
+            grounded = false;
         }
     }
 }
