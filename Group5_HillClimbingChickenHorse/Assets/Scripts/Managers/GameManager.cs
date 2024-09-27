9c7f20550ec4f56738bbccb294e9661db97f70c3
CayCall <131669034+CayCall@users.noreply.github.com>
2024-09-27 21:38:58 +0200

    changes

---
+++ Group5_HillClimbingChickenHorse/Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject pnlItems;
+    private int pnlTimer = 0;
+
+   [SerializeField] private bool isChosen;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+            
+    }
+}
