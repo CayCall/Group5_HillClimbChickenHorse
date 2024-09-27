adaea8fbc8c20fcf17354c5095c62af08a4e4b37
CayCall <131669034+CayCall@users.noreply.github.com>
2024-09-27 23:47:49 +0200

    made some changes, started basic menu

--- Group5_HillClimbingChickenHorse/Assets/Scripts/Car/Cursor script.cs
+++ Group5_HillClimbingChickenHorse/Assets/Scripts/Car/Cursor script.cs
@@ -3,28 +3,67 @@ using UnityEngine;
 
 namespace DefaultNamespace
 {
-    public class Cursor_script : MonoBehaviour
+   public class Cursor_script : MonoBehaviour
     {
-        [SerializeField] private PlayerInputs playerInputs;
-        [SerializeField] private Transform cursorObject;
+        [SerializeField] private PlayerInputs playerInputs; // Assuming you have a PlayerInputs script handling controller input
+        [SerializeField] public Transform cursorObject;
         [SerializeField] private float moveSpeed;
+        
+        private GameObject selectedObject;
+        private bool isObjectSelected = false;
 
         private void Update()
         {
             MoveCursor();
+            CheckObjectSelection();
         }
 
         public void MoveCursor()
         {
             if (cursorObject != null)
             {
-                var newPos = new Vector2((cursorObject.position.x 
-                                          + playerInputs.cursorMoveDirction.x) 
-                                         * moveSpeed, 
-                    (cursorObject.position.y 
-                     + playerInputs.cursorMoveDirction.y) 
+                var newPos = new Vector2((cursorObject.position.x
+                                          + playerInputs.cursorMoveDirction.x)
+                                         * moveSpeed,
+                    (cursorObject.position.y
+                     + playerInputs.cursorMoveDirction.y)
                     * moveSpeed);
                 cursorObject.position = newPos;
+            }
+            
+            if (isObjectSelected && selectedObject != null)
+            {
+                selectedObject.transform.position = cursorObject.position;
+            }
+        }
+
+        public void CheckObjectSelection()
+        {
+            // gonna use 'A or  X' button (or another button)for selecting the object
+            if (cursorObject != null && Input.GetButtonDown("SelectButton")) 
+            {
+                RaycastHit2D hit = Physics2D.Raycast(cursorObject.position, Vector2.zero);
+
+                if (hit.collider != null && hit.collider.CompareTag("Selectable"))
+                {
+                    Debug.Log("hit:" + gameObject.name);
+                    selectedObject = hit.collider.gameObject;
+                    isObjectSelected = true;
+                    selectedObject.GetComponent<Collider2D>().enabled = false;
+                    
+                    FindObjectOfType<GameManager>().ClosePanel();
+                }
+            }
+        }
+
+        public void InstantiateSelectedObject(Vector2 position)
+        {
+            if (selectedObject != null)
+            {
+                Instantiate(selectedObject, position, Quaternion.identity);
+                Destroy(selectedObject);
+                isObjectSelected = false;
+                selectedObject = null;
             }
         }
     }
--- Group5_HillClimbingChickenHorse/Assets/Scripts/Managers/GameManager.cs
+++ Group5_HillClimbingChickenHorse/Assets/Scripts/Managers/GameManager.cs
@@ -1,22 +1,81 @@
 using System.Collections;
 using System.Collections.Generic;
+using DefaultNamespace;
+using Unity.VisualScripting;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
-    public GameObject pnlItems;
-    private int pnlTimer = 0;
 
-   [SerializeField] private bool isChosen;
-    // Start is called before the first frame update
+
+    [SerializeField] private bool isChosen;
+
+// temp for milestone 1
+    [Header("Temp")] 
+    public GameObject cursor; 
+    public GameObject pnlItems;
+    private float pnlTimer = 5;
+    private float currentpnlTimer;
+    private float countdownInterval = 1f; 
+    private float nextTime;
+    public TextMeshProUGUI timerText; 
+    
+    
+    
     void Start()
     {
+        if (pnlItems != null)
+        {
+            pnlItems.SetActive(true);
+        }
+
+        currentpnlTimer = pnlTimer;
+        nextTime = Time.time + countdownInterval;
         
     }
-
-    // Update is called once per frame
+    
     void Update()
     {
-            
+        // 'B' button or "Fire2" input for placing 
+        if (isChosen && Input.GetButtonDown("PlaceButton"))
+        {
+            Vector2 cursorPosition = FindObjectOfType<Cursor_script>().cursorObject.position;
+            FindObjectOfType<Cursor_script>().InstantiateSelectedObject(cursorPosition);
+            isChosen = false;
+        }
+        if ( currentpnlTimer > 0 && Time.time >= nextTime)
+        {
+            currentpnlTimer--; 
+            nextTime = Time.time + countdownInterval; 
+          
+        }
+
+        if ( currentpnlTimer <= 0)
+        {
+            currentpnlTimer = 0;
+            ClosePanel();
+        }
+
+        if (currentpnlTimer == 0)
+        {
+            cursor.SetActive(false);   
+        }
+        
+        UpdateTimerDisplay();
+        Debug.Log(currentpnlTimer);
+    }
+    void UpdateTimerDisplay()
+    {
+        timerText.text = "Choose your item before the placement phase: " + currentpnlTimer.ToString(); // Display the timer
+    }
+
+    public void ClosePanel()
+    {
+        if (pnlItems != null)
+        {
+            pnlItems.SetActive(false);
+            isChosen = true;
+        }
     }
 }
