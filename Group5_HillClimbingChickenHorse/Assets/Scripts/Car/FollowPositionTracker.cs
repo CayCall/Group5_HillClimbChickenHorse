e530f3943be19de2a8c400abdc0862aa823dcebf
CayCall <131669034+CayCall@users.noreply.github.com>
2024-11-21 17:40:20 +0200

    made a few changes

--- Group5_HillClimbingChickenHorse/Assets/Scripts/Car/FollowPositionTracker.cs
+++ Group5_HillClimbingChickenHorse/Assets/Scripts/Car/FollowPositionTracker.cs
@@ -12,7 +12,7 @@ public class FollowPositionTracker : MonoBehaviour
         if (!rb) rb = GetComponent<Rigidbody2D>();
     }
 
-    void FixedUpdate()
+    void Update()
     {
         rb.MovePosition(positionTracker.position);
     }
